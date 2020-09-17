c77a7a59697b53df4268bc16bbbbe77e720000de
Felipe Rosa <flp.far@gmail.com>
2020-09-17 13:19:49 -0300

    Add seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.create!(name: 'User', email: 'user@user.com', password: '123123', daily_goal: 1)
+Day.create!(date: Date.current, reviewed: 6, learned: 1, user: user)
+Day.create!(date: Date.current - 1, reviewed: 4, learned: 2, user: user)
+Day.create!(date: Date.current - 2, reviewed: 6, learned: 1, user: user)
+Day.create!(date: Date.current - 3, reviewed: 1, learned: 0, user: user)
+Day.create!(date: Date.current - 4, reviewed: 0, learned: 0, user: user)
+Day.create!(date: Date.current - 5, reviewed: 0, learned: 0, user: user)
